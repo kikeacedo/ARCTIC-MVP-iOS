67b57bf59d15b5d733469af991980f56bd556dae
quiqueacedo <kike.acedo@gmail.com>
2015-03-18 16:22:54 +0100

    Initial Commit

---
+++ SkiSensor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkiSensor
+//
+//  Created by Enrique Acedo Dorado on 18/3/15.
+//  Copyright (c) 2015 Enrique Acedo Dorado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SkiSensor/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SkiSensor
+//
+//  Created by Enrique Acedo Dorado on 18/3/15.
+//  Copyright (c) 2015 Enrique Acedo Dorado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
